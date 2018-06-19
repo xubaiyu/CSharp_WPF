0b28e75d1f1c8fca51db955bbd40d9141028b563
xubaiyu <xubaiyu@163.com>
2018-06-19 17:47:31 +0800

    basic 1

---
+++ CustomListBox/CustomListBox/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomListBox
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<string> a = new List<string>();
+            for (int i = 0; i < 10; i++)
+            {
+                a.Add("abc" + i);
+                a.Add("123" + i);
+                a.Add("ere" + i);
+
+            }
+
+            myListBox.ItemsSource = a;
+        }
+    }
+}
