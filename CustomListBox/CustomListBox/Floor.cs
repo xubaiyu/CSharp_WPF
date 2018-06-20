593dd66d07f168a7b3c4f0a292e378d484d4b84f
xubaiyu <xubaiyu@163.com>
2018-06-20 16:03:55 +0800

    basic ok

---
+++ CustomListBox/CustomListBox/Floor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListBox
+{
+    class Floor
+    {
+        public string floorId { get; set; }
+
+        public string floorName { get; set; }
+    }
+}
--- CustomListBox/CustomListBox/MainWindow.xaml.cs
+++ CustomListBox/CustomListBox/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,16 +24,44 @@ namespace CustomListBox
         public MainWindow()
         {
             InitializeComponent();
-            List<string> a = new List<string>();
-            for (int i = 0; i < 2; i++)
+            List<Floor> a = new List<Floor>();
+            for (int i = 20; i > 0; i--)
             {
-                a.Add("abc" + i);
-                a.Add("123" + i);
-                a.Add("ere" + i);
+                a.Add(new Floor()
+                {floorName =  i+"层",floorId = i+""}
+                );
+               
 
             }
 
+            DataTable floors = new DataTable();
+            floors.Columns.Add("FloorName");
+            floors.Columns.Add("floorId");
+
+            for (int i = 0; i < 2; i++)
+            {
+                floors.Rows.Add( i + "层", i+"");
+
+            }
+
+            
+
             myListBox.ItemsSource = a;
+        }
+
+        private void Image_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Image_MouseLeave(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
